506bf591bc885aabc6efb1e856f9a004eb7e6e65
Ildar Karimov <thevery@yandex-team.ru>
2016-07-28 17:21:53 +0300

    Stub for app UI

--- app/src/main/java/com/thevery/yamblz/MainActivity.java
+++ app/src/main/java/com/thevery/yamblz/MainActivity.java
@@ -1,7 +1,11 @@
 package com.thevery.yamblz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void translate(View view) {
+        final String text = ((EditText) findViewById(R.id.et)).getText().toString();
+        ((TextView) findViewById(R.id.tv)).setText(translate(text));
+    }
+
+    @NonNull
+    private String translate(@NonNull String source) {
+        return source.toUpperCase();
     }
 }
