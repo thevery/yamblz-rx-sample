7cb788479ddad114556e07320da1365aeb0fbae8
Ildar Karimov <thevery@yandex-team.ru>
2016-07-28 17:49:20 +0300

    Rx-ed and retrofit-ed app

--- app/src/main/java/com/thevery/yamblz/MainActivity.java
+++ app/src/main/java/com/thevery/yamblz/MainActivity.java
@@ -7,21 +7,75 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.thevery.yamblz.api.YandexTranslateApi;
+import com.thevery.yamblz.api.YandexTranslateReponse;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
+
 public class MainActivity extends AppCompatActivity {
+
+    private YandexTranslateApi translateApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final OkHttpClient client = new OkHttpClient();
+        final OkHttpClient.Builder retrofitClientBuilder = client.newBuilder();
+
+        if (BuildConfig.DEBUG) {
+            final HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            retrofitClientBuilder.addInterceptor(interceptor);
+            retrofitClientBuilder.addNetworkInterceptor(interceptor);
+        }
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addConverterFactory(JacksonConverterFactory.create())
+                .baseUrl("https://translate.yandex.net/api/v1.5/tr.json/")
+                .client(retrofitClientBuilder.build())
+                .build();
+
+        translateApi = retrofit.create(YandexTranslateApi.class);
     }
 
     public void translate(View view) {
         final String text = ((EditText) findViewById(R.id.et)).getText().toString();
-        ((TextView) findViewById(R.id.tv)).setText(translate(text));
+        startTranslation(text);
+//        ((TextView) findViewById(R.id.tv)).setText(translate(text));
     }
 
-    @NonNull
-    private String translate(@NonNull String source) {
-        return source.toUpperCase();
+//    @NonNull
+//    private String translate(@NonNull String source) {
+//        return translateApi.translate(YandexTranslateApi.YANDEX_API_KEY, source, "ru").toBlocking().value().text[0];
+//    }
+
+    private Subscription startTranslation(@NonNull String source) {
+        return translateApi.translate(YandexTranslateApi.YANDEX_API_KEY, source, "ru")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .map(new Func1<YandexTranslateReponse, String>() {
+                    @Override
+                    public String call(YandexTranslateReponse yandexTranslateReponse) {
+                        return yandexTranslateReponse.text[0];
+                    }
+                })
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        ((TextView) findViewById(R.id.tv)).setText(s);
+                    }
+                });
     }
 }
---
+++ app/src/main/java/com/thevery/yamblz/api/YandexTranslateApi.java
@@ -0,0 +1,20 @@
+package com.thevery.yamblz.api;
+
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import rx.Single;
+
+/**
+ * API interface for https://tech.yandex.ru/translate/doc/dg/reference/translate-docpage/
+ */
+public interface YandexTranslateApi {
+    String YANDEX_API_KEY = "trnsl.1.1.20160728T140517Z.5cc52d9c03643fb3.90d08d77a81e382c545d2cd5619babd370d5b52f";
+
+    @GET("translate")
+    Single<YandexTranslateReponse> translate(
+            @Query("key") String key,
+            @Query("text") String text,
+            @Query("lang") String lang
+    );
+
+}
